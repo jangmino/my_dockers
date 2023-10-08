c402dec8523d5e47a5cc5d3213de40ef6d03f783
Jangmin Oh <jangmin.oh@sungshin.ac.kr>
2023-10-08 15:39:43 +0900

    peft 준비

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# 최신 버전: 2023-10-06
+docker build -t jangminnature/peft:v1.1a-torch2.1-cu118 --build-arg UID=$UID --build-arg USER_NAME=$USER -f Dockerfile.peft.v1.1 ./
 
 # 최신 버전: 2023-09-11
-docker build -t jangminnature/peft:v1.0-23.08-py3 --build-arg UID=$UID --build-arg USER_NAME=$USER -f Dockerfile.peft.v1.1 ./
+# docker build -t jangminnature/peft:v1.0-23.08-py3 --build-arg UID=$UID --build-arg USER_NAME=$USER -f Dockerfile.peft.v1.1 ./
 
 # 최신 버전: 2023-09-07
 # docker build -t jangminnature/peft:v1.0-23.05-py3 --build-arg UID=$UID --build-arg USER_NAME=$USER -f Dockerfile.peft.v1.0 ./
